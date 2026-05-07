f4e8fbce74aafb1b58635b380a7c0884a5701369
Yuliana Samboni <samboniandryyuliana@gmail.com>
2026-05-06 21:26:24 -0500

    Se añadio ConfigModulo al archivo app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { CategoriasModule } from './categorias/categorias.module';
 import { ProductosModule } from './productos/productos.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/catalogo'),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     CategoriasModule,
-    ProductosModule
+    ProductosModule,
   ],
-
 })
 export class AppModule {}
